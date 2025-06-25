1d6f9499c6823d2a3c83055cf051e1301c6e5ae7
Kanav Dahat <kanavdahat55@gmail.com>
2025-06-25 22:54:49 +0530

    Started with strings

---
+++ JavaStrings/src/JavaStrings/JavaStrings.java
@@ -0,0 +1,11 @@
+package JavaStrings;
+import java.util.Scanner;
+public class JavaStrings {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String m = s.next();
+		char d= m.charAt(0);
+		System.out.println(d);
+		System.out.println(m);
+	}
+}
